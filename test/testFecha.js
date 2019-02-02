72e6579c1be8ff53a19ff79b6aa4effc27ec9ba8
MarAl15 <maralguacil@correo.ugr.es>
2019-02-03 00:02:49 +0100

    Comentarios test validación fecha #23

--- test/testFecha.js
+++ test/testFecha.js
@@ -3,16 +3,19 @@ var request = require('supertest'),
 	assert = require('assert');
 
 describe( "Comprobación fecha", function() {
+	// Fecha correcta al ser posterior al día actual 
 	it('Fecha correcta', function (done) {
 		assert.equal(true, cf.comprobarFecha(10, 10, 2030));
 		done();
 	});
 	
+	// Fecha inválida debido a que el año solo tiene 12 meses
 	it('Fecha invalida', function (done) {
-		assert.equal(false, cf.comprobarFecha(10, 13, 2030));
+		assert.equal(false, cf.comprobarFecha(10, 15, 2030));
 		done();
 	});
 	
+	// Fecha invalida debido a que es anterior a la fecha actual
 	it('Fecha anterior', function (done) {
 		assert.equal(false, cf.comprobarFecha(10, 10, 2010));
 		done();
